5d7449b763a2ffebc34b505760e0057aa9eb7384
Ganna Pavlova <ganna@unumotors.com>
2021-02-01 10:40:13 +0100

    Add redux-persist for login

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,50 @@
-import { configureStore, Action } from '@reduxjs/toolkit'
+import { configureStore, combineReducers, Action, getDefaultMiddleware } from '@reduxjs/toolkit'
 import { ThunkAction } from 'redux-thunk'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import loginReducer, { LoginState } from '../features/login/loginSlice'
 import productsReducer, { ProductsState } from '../features/products/productsSlice'
 
-const store  = configureStore({
-  reducer: {
-    login: loginReducer,
-    products: productsReducer,
-  }
+// the solution to make redux-toolkit and redux-persist work together
+// taken from here: https://github.com/reduxjs/redux-toolkit/issues/121
+const reducers = combineReducers({
+  login: loginReducer,
+  products: productsReducer
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['login']
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  // the solution is taken from here: https://github.com/rt2zz/redux-persist/issues/988#issuecomment-552242978
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  }),
+  devTools: process.env.NODE_ENV !== 'production',
+})
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppThunk = ThunkAction<void, LoginState, unknown, Action<string>>;
 
-export default store
+export {store, persistor}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './app/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './app/store'
 import 'antd/dist/antd.css';
 import App from './App';
 import './index.css';
@@ -9,7 +10,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('app') as HTMLElement
